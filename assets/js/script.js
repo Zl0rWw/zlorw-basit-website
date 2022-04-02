cd8ba13fe6550ef367841f78ce0cfa83e467b2d0
Zl0rWw <skyguard2121@gmail.com>
2022-04-02 13:27:08 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* ============= TYPİNG ANIMATION ============= */
+var typed6 = new Typed('.types', {
+    strings: ['Turkish', 'Human', 'Cat', 'Developer', 'Coder', 'Designer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
